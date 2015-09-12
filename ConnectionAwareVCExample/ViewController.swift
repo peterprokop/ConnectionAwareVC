f2daca6ec677a5cd2be900f0ce41f655a4e659e0
Peter Prokop <prokop.petr@gmail.com>
2015-09-12 19:27:10 +0300

    Initial Commit

---
+++ ConnectionAwareVCExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectionAwareVCExample
+//
+//  Created by Peter Prokop on 12/09/15.
+//  Copyright (c) 2015 Peter Prokop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
